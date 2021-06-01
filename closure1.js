7a0268eebac6edc4295d5f66833c9a3edbb08756
Yulia Rezanova <qwerty959@gmail.com>
2021-06-01 23:54:34 +0300

    01.06.21

---
+++ closure1.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/**Напишите функцию addNum(n), которая вернёт другую функцию. Возвращенная функция 
+ * должна складывать получаемый аргумент с аргументом n возвращающей функции.
+Пояснения:
+- внешняя функция addNum(n) имеет параметр n
+- возвращаемая из addNum функция имеет параметр m
+- пример использования:
+const add = addNum(5);
+const result = add(10); // => 15 */
+
+function addNum(n) {
+    return function (m) {
+        return m + n;
+    };
+}
+
+// debugger;
+const add = addNum(5);
+const result = add(10);
+console.log(result);
